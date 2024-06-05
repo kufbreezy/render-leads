34623952fa32f03c9511359f1af647f1ae8bcb2a
Kufre | DevSecOps <kufre@businessflaws.com>
2024-06-05 01:21:14 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const { scrapeLogic } = require("./scrapeLogic");
+const { googleLogic } = require("./googleLogic");
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/scrape", (req, res) => {
+  scrapeLogic(res);
+});
+
+app.get("/google", (req, res) => {
+  googleLogic(res);
+});
+
+app.get("/", (req, res) => {
+  res.send("Render Puppeteer server is up and running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
